f818a085d2047672539abd7fd949081212b94495
Adithya Pai B <paiadithya26@gmail.com>
2021-09-21 21:02:37 +0530

    Added Link support

--- server.js
+++ server.js
@@ -9,41 +9,34 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.get("/", (req, res) => {
-  const code = `Lorem ipsum dolor sit amet consectetur 
-adipisicing elit. Fugit corrupti error expedita ratione 
-velit. Voluptas laboriosam iste, obcaecati suscipit magnam,
-aperiam recusandae magni 
-earum asperiores enim labore? Modi, tempore fugiat.`;
-  res.render("code-display", { code , language: "plaintext" });
+  const code = `Hello`;
+  res.render("code-display", { code, language: "plaintext" });
 });
 app.get("/new", (req, res) => {
-    res.render('new')
-
+  res.render("new");
 });
-app.post("/save", async(req, res) => { 
-    const value =  await req.body.value;
-    try{
-       const document = await Document.create({ value })
-       res.redirect(`/${document.id}`);
-
-    }
-    catch
-    {
-        res.render("new", {value})
-    }
-
-})
-app.get("/:id", async(req, res) => {
-    const id = req.params.id;
-    try{
-        const document = await Document.findById(id)
-        res.render("code-display", { code: document.value })
-
-    }
-    catch(e)
-    {
-        res.render("/")
-    }
+let detectURLs = (string) =>{
+    let hello =  string.replace(/(\b(https?|ftp|file):\/\/[\-A-Z0-9+&@#\/%?=~_|!:,.;]*[\-A-Z0-9+&@#\/%=~_|])/img, '<a href="$1">$1</a>');
+    return hello;
+  }
+app.post("/save", async (req, res) => {
+  let value = await req.body.value;
+  value = await detectURLs(value);
+  try {
+    const document = await Document.create({ value });
+    res.redirect(`/${document.id}`);
+  } catch {
+    res.render("new", { value });
+  }
+});
+app.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const document = await Document.findById(id);
+    res.render("code-display", { code: document.value });
+  } catch (e) {
+    res.render("/");
+  }
 });
 
 app.listen(3000);
