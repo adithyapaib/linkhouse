3ed40946374f131ce7af188d20f1990c994d633b
Adithya Pai B <paiadithya26@gmail.com>
2021-09-22 10:34:16 +0530

    index.ejs added

--- server.js
+++ server.js
@@ -1,30 +1,33 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Document = require("./models/Documents");
+
+
 require('dotenv').config()
 mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true });
-
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+
+
 app.get("/", (req, res) => {
-  const code = `Hello`;
-  res.render("code-display", { code, language: "plaintext" });
+  res.render("index",);
 });
 app.get("/new", (req, res) => {
   res.render("new");
 });
 
 
-let detectURLs = async (string) => await string.replace(/(\b(https?|ftp|file):\/\/[\-A-Z0-9+&@#\/%?=~_|!:,.;]*[\-A-Z0-9+&@#\/%=~_|])/img, '</code> <a href="$1">$1</a> <code> </code>');
+let detectURLs = async (string) => await string.replace(/(\b(https?|ftp|file):\/\/[\-A-Z0-9+&@#\/%?=~_|!:,.;]*[\-A-Z0-9+&@#\/%=~_|])/img, '<a href="$1">$1</a><br>');
 
 
 
 app.post("/save", async (req, res) => {
 let body = await req.body.value;
 body = await detectURLs(body);
-let value =  await `<code>${body} </code>`
+let value =  await `<p class="saved" >${body} </p>`
   try {
     const document = await Document.create({ value });
     res.redirect(`/${document.id}`);
